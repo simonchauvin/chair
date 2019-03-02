3735ab0561fabbef90e969a0123d2af7aacdc941
Simon Chauvin <contact@simonchauvin.com>
2019-03-02 12:13:50 +0100

    Added GameManager with basic methods and player.

---
+++ Assets/Scripts/Global/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
+    }
+
+    private Player player;
+
+    private bool ready;
+
+
+    void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        player = FindObjectOfType<Player>();
+
+        player.Init();
+
+        ready = true;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Restart()
+    {
+        Init();
+    }
+
+    public bool IsReady()
+    {
+        return ready;
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public void Init()
+    {
+
+    }
+
+    void Update()
+    {
+        if (GameManager.Instance.IsReady())
+        {
+
+        }
+    }
+}
